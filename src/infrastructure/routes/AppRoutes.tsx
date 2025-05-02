c11e2c6e9cbe0da19f3ed11067ce5b737bbddc29
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2025-05-02 13:05:40 -0500

    v0.0.1 - basic deployment

--- src/infrastructure/routes/AppRoutes.tsx
+++ src/infrastructure/routes/AppRoutes.tsx
@@ -1,8 +1,8 @@
 import {createBrowserRouter} from "react-router";
 import Layout from "../../presentation/layout/Layout.tsx";
 import Home from "../../presentation/pages/Home.tsx";
-import Tours from "../../presentation/pages/Tours.tsx";
-import Login from "../../presentation/pages/Login.tsx";
+// import Tours from "../../presentation/pages/Tours.tsx";
+// import Login from "../../presentation/pages/Login.tsx";
 import Error404 from "../../presentation/pages/Error404.tsx";
 
 export const router = createBrowserRouter([
