2a5189b6bd45ebe4fd6d8aec7877e5f3010d62c6
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2025-05-03 13:46:11 -0500

    v0.0.2 Login, Register, Recover & Tours

--- src/infrastructure/routes/AppRoutes.tsx
+++ src/infrastructure/routes/AppRoutes.tsx
@@ -1,8 +1,8 @@
 import {createBrowserRouter} from "react-router";
 import Layout from "../../presentation/layout/Layout.tsx";
 import Home from "../../presentation/pages/Home.tsx";
-// import Tours from "../../presentation/pages/Tours.tsx";
-// import Login from "../../presentation/pages/Login.tsx";
+import Tours from "../../presentation/pages/Tours.tsx";
+import Login from "../../presentation/pages/Login.tsx";
 import Error404 from "../../presentation/pages/Error404.tsx";
 import Register from "../../presentation/pages/Register.tsx";
 import RecoverPassword from "../../presentation/pages/RecoverPassword.tsx";
