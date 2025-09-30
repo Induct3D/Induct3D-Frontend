b8a5bbf11b2ba0a9c085c0373be85154d7bc70de
Flavio Villacorta <139486497+Flavicox@users.noreply.github.com>
2025-09-30 17:21:18 -0500

    Ajuste

--- src/infrastructure/store/store.ts
+++ src/infrastructure/store/store.ts
@@ -10,7 +10,3 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(induct3dApi.middleware)
 })
-
-// Tipos para TS
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
